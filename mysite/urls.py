4226607cc73fa0009edc414f159cd245b4ff81c0
shubh232 <shubh232@gmail.com>
2016-06-28 01:24:53 +0530

    Check commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from mysite.views import Index
+from mysite.views import Index,Check
 
 admin.autodiscover()
 
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', Index.as_view(), name='index'),
-
+    url(r'^check/$',Check.as_view(),name = 'check')
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,9 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django import get_version
 
+class Check(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Running Django ' + str(get_version()) + " on Shubh")
 
 class Index(View):
 
