c3907c230814bcac2cb8e15857cb4d6134f132ef
shubh232 <shubh232@gmail.com>
2016-06-30 00:36:24 +0530

    index fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,13 +7,17 @@ from django.contrib.auth.views import login
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^reg/$',Regis.as_view(),name='register'),
     url(r'^$', Index.as_view(), name='index'),
     url(r'^check/',Check.as_view(),name='check'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$',login,{'template_name': 'login.html'}),
-    url(r'^accounts/profile/$',TemplateView.as_view(template_name='profile.html')),
-    url(r'^accounts/profile/newart/$',NewArt.as_view()),
+    url(r'^accounts.profile/$',TemplateView.as_view(template_name='profile.html')),
+    url(r'^profile/newart/$',NewArt.as_view()),
 )
 
 
